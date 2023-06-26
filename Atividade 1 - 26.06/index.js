dc20c7e5b275126dd82a6f5c265d00dec5e426e3
Danielly Egito <75611712+dEgito@users.noreply.github.com>
2023-06-26 16:23:14 -0300

    Feat: atividade de fixação 1 - 26/06 (Qua)

---
+++ Atividade 1 - 26.06/index.js
@@ -0,0 +1,43 @@
+// PRÁTICA START - 26/06/2023
+// TIPOS DE VARIÁVEIS
+
+// Number
+let agr = 25;
+let height = 1.55;
+
+// String
+let name = "Rasângela Campos";
+let address = "Rua Gevársio Pires, 123";
+
+//Boolean
+let isRaining = true;
+let isSun = false;
+
+//Array
+let numbers = [1, 2, 3, 4, 5];
+
+let colors = [
+  "vermelho", 
+  "azul", 
+  "verde"
+];
+
+//Object
+let person = { 
+  nome: "Maria", 
+  idade: 30 
+};
+
+let books = { 
+  titulo: "Harry Potter e a Pedra Filosofal", 
+  autor: "JK Rowling" 
+};
+
+// Undefined
+let surname;
+let car;
+
+//Null
+let value = null;
+let result = null;
+
