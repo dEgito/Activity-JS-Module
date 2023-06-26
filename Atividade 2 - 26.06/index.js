f0ee550e7ccdd9cccd3137b14f840b600aa54ac6
Danielly Egito <75611712+dEgito@users.noreply.github.com>
2023-06-26 16:30:01 -0300

    feat: Atividade de fixação 2 - 26-06 (Qua)

---
+++ Atividade 2 - 26.06/index.js
@@ -0,0 +1,14 @@
+// PRÁTICA START - 26/06/2023
+// TIPOS DE VARIÁVEIS
+
+//VAR
+var product = "Mesa de Mármore";
+var price = 289.89;
+
+//LET
+let book = "IT: A Coisa";
+let weight = 7.5;
+
+//CONST
+const pi = 3.14159;
+const euler = 2.71828;
